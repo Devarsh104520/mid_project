004902817c68a13d82c9719b7814080ce74d3190
Devarsh104520 <158536411+Devarsh104520@users.noreply.github.com>
2024-02-29 20:44:09 -0500

    third commit

--- dosa_script.py
+++ dosa_script.py
@@ -1,22 +1,17 @@
 import json
 
 def process_orders(input_file):
-    # Initialize dictionaries for customers and items
     customers = {}
     items = {}
 
-    # Read orders from the input file
     with open(input_file, 'r') as file:
         orders_data = json.load(file)
 
-    # Process each order
     for order in orders_data:
-        # Extract and update customer information
         phone = order['phone']
         name = order['name']
         customers[phone] = name
         
-        # Extract and update item information
         for item in order['items']:
             item_name = item['name']
             item_price = item['price']
@@ -26,14 +21,12 @@ def process_orders(input_file):
             else:
                 items[item_name]['orders'] += 1
 
-    # Write the processed data to new JSON files
     with open('customers.json', 'w') as file:
         json.dump(customers, file, indent=4)
     
     with open('items.json', 'w') as file:
         json.dump(items, file, indent=4)
 
-# Assuming 'example_orders.json' is the input file
 input_file_path = r'C:\Users\kunal\OneDrive\Desktop\mid_project\example_orders.json'
 process_orders(input_file_path)
 
